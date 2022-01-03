792db05a010e6aef93ed9db352347d0a49b6d022
DESKTOP-SIRV9EL\steve <steveballantine@gmail.com>
2022-01-03 21:03:57 +0000

    Add additional map options

--- BlazorApp/Data/Map.cs
+++ BlazorApp/Data/Map.cs
@@ -22,6 +22,84 @@ namespace SiRandomizer.Data
             MaxCount = 6,
             DifficultyModifier = 3
         };
+        public static Map Archipelago = new Map() {
+            Name = "Archipelago",
+            MinCount = 2,
+            MaxCount = 6,
+            DifficultyModifier = 1
+        };
+        public static Map Fragment = new Map() {
+            Name = "Fragment",
+            MinCount = 2,
+            MaxCount = 2,
+            DifficultyModifier = 0
+        };
+        public static Map OppositeShores = new Map() {
+            Name = "Opposite Shores",
+            MinCount = 2,
+            MaxCount = 2,
+            DifficultyModifier = 0
+        };
+        public static Map Coastline = new Map() {
+            Name = "Coastline",
+            MinCount = 2,
+            MaxCount = 6,
+            DifficultyModifier = 0
+        };
+        public static Map Sunrise = new Map() {
+            Name = "Sunrise",
+            MinCount = 3,
+            MaxCount = 3,
+            DifficultyModifier = 0
+        };
+        public static Map Leaf = new Map() {
+            Name = "Leaf",
+            MinCount = 4,
+            MaxCount = 4,
+            DifficultyModifier = 0
+        };
+        public static Map Snake = new Map() {
+            Name = "Snake",
+            MinCount = 3,
+            MaxCount = 6,
+            DifficultyModifier = 0
+        };
+        public static Map V = new Map() {
+            Name = "V",
+            MinCount = 5,
+            MaxCount = 5,
+            DifficultyModifier = 0
+        };
+        public static Map Snail = new Map() {
+            Name = "Snail",
+            MinCount = 5,
+            MaxCount = 5,
+            DifficultyModifier = 0
+        };
+        public static Map Peninsula = new Map() {
+            Name = "Peninsula",
+            MinCount = 5,
+            MaxCount = 5,
+            DifficultyModifier = 0
+        };
+        public static Map Star = new Map() {
+            Name = "Star",
+            MinCount = 6,
+            MaxCount = 6,
+            DifficultyModifier = 0
+        };
+        public static Map Flower = new Map() {
+            Name = "Flower",
+            MinCount = 6,
+            MaxCount = 6,
+            DifficultyModifier = 0
+        };
+        public static Map Caldera = new Map() {
+            Name = "Caldera",
+            MinCount = 6,
+            MaxCount = 6,
+            DifficultyModifier = 0
+        };
 
         public int MinCount {get; set;}
         public int MaxCount {get; set;}
