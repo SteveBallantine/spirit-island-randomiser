5fdd19a90b96c5efb61a550f9730bf0d6218938b
DESKTOP-SIRV9EL\steve <steveballantine@gmail.com>
2022-03-19 10:37:36 +0000

    Don't show setup instructions if none are needed

--- BlazorApp/Data/SetupResult.cs
+++ BlazorApp/Data/SetupResult.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace SiRandomizer.Data
 {
@@ -9,5 +10,22 @@ namespace SiRandomizer.Data
         public GameSetup Setup { get; set; }
         public long DifficultyOptionsConsidered { get; set; }
         public long BoardSetupOptionsConsidered { get; set; }
+
+        public bool ShowInstructionsAdditionalBoard =>
+            Setup.BoardSetups.Any(b => b.SpiritAspect == null);
+        public bool ShowInstructionsAdditionalAdversary =>
+            Setup.HasSupportingAdversary;
+        public bool ShowInstructionsNonStandardMap =>
+            Setup.Map.Name != Map.Standard &&
+            Setup.Map.Name != Map.ThematicNoTokens &&
+            Setup.Map.Name != Map.ThematicTokens;
+        public bool ShowInstructionsArchipelago =>
+            Setup.Map.Name == Map.Archipelago;
+
+        public bool ShowInstructions => 
+            ShowInstructionsAdditionalAdversary ||
+            ShowInstructionsAdditionalBoard ||
+            ShowInstructionsArchipelago ||
+            ShowInstructionsNonStandardMap;
     }
 }
